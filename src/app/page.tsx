4e443e3d643c926e46dcfc5fa4be26fb2ab18cb7
HELAL15 <1ahmedhelal1@gmail.com>
2025-05-05 15:53:28 +0300

    add next-intl axios js-cookies nextjs-toploader , and create folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link';
+
 import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
+
+import LanguageSwitcher from '../components/common/LanguageSwitcher';
 
 /**
  * The main page component that renders the HomePage component.
@@ -6,7 +10,13 @@ import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
  * @returns {JSX.Element} The rendered HomePage component.
  */
 const Page = () => {
-    return <HomePage />;
+    return (
+        <>
+            <LanguageSwitcher />
+            <HomePage />
+            <Link href={'/about'}>fff</Link>
+        </>
+    );
 };
 
 export default Page;
